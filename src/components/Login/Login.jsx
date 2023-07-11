b204e11552c4a14b7e78c5c010db62cb2577784b
Mufaddal Mustafa <mufaddal.mustafa@xloopdigital.com>
2023-07-11 14:21:18 +0500

    input and login component modified

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,8 +1,30 @@
-import React from 'react'
+import React, { useState } from 'react'
+import InputField from '../InputField/InputField'
+import styles from './Login.module.css'
 
 const Login = () => {
+
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+
+  const emailHandler = (value) => {
+    setEmail(value)
+  }
+
+  const passwordHandler = (value) => {
+    setPassword(value)
+  }
+
   return (
-    <div>Login</div>
+    <div>
+      <div className={styles.inputContainer}>
+        <InputField placeholder='E-mail Address' value={email} handler={emailHandler} type='email' required={true} />
+      </div>
+      <div className={styles.inputContainer}>
+        <InputField placeholder='Password' value={password} handler={passwordHandler} type='password' required={true} />
+      </div>
+      
+    </div>
   )
 }
 
