58a12fed35f892e5b38c86ba60b5327bc75427c1
Aiman Zaheer <Aiman Zaheer@DESKTOP-9TI8MFU>
2023-07-11 01:16:52 -0700

    Navigation done

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,11 +1,12 @@
+import styled from './Navigation.module.css'
 import React from 'react'
 import { Link } from 'react-router-dom'
 
 const Navigation = () => {
   return (
-    <div>
-        <Link to='/login' >Login</Link>
-        <Link to='/register' >Register</Link>
+    <div className={styled.Navigation}>
+        <Link className={styled.login}  to='/login' >Login</Link>
+        <Link className={styled.register} to='/register' >Register</Link>
     </div>
   )
 }
