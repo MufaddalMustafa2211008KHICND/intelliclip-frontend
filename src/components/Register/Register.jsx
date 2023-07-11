9c8c94156b0408e3d5b3cf782b93f580c9268c9f
Mufaddal Mustafa <mufaddal.mustafa@xloopdigital.com>
2023-07-11 14:44:40 +0500

    register component modified

--- src/components/Register/Register.jsx
+++ src/components/Register/Register.jsx
@@ -1,8 +1,37 @@
-import React from 'react'
+import React, { useState } from 'react'
+import InputField from '../InputField/InputField'
+import styles from './Register.module.css'
 
 const Register = () => {
+
+  const [name, setName] = useState('')
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+
+  const nameHandler = (value) => {
+    setName(value)
+  }
+  
+  const emailHandler = (value) => {
+    setEmail(value)
+  }
+
+  const passwordHandler = (value) => {
+    setPassword(value)
+  }
+
   return (
-    <div className='register'>Register</div>
+    <div>
+      <div className={styles.inputContainer}>
+        <InputField placeholder='Full Name' value={name} handler={nameHandler} type='text' required={true} />
+      </div>
+      <div className={styles.inputContainer}>
+        <InputField placeholder='E-mail Address' value={email} handler={emailHandler} type='email' required={true} />
+      </div>
+      <div className={styles.inputContainer}>
+        <InputField placeholder='Password' value={password} handler={passwordHandler} type='password' required={true} />
+      </div>
+    </div>
   )
 }
 
