822da8243306ad41e97b161f77691717d40a7e90
Mufaddal Mustafa <mufaddal.mustafa@xloopdigital.com>
2023-07-13 11:29:36 +0500

    toggle password visibility option added

--- src/components/InputField/InputField.jsx
+++ src/components/InputField/InputField.jsx
@@ -1,8 +1,17 @@
-import React from 'react'
-import { TextField } from '@mui/material'
+import React, { useState } from 'react'
+import { IconButton, InputAdornment, TextField } from '@mui/material'
 import './InputField.css'
+import { Visibility, VisibilityOff } from '@mui/icons-material'
 
 const InputField = ({ placeholder, value, handler, type, required }) => {
+
+  const [showPassword, setShowPassword] = useState(false)
+
+  const handleClickShowPassword = () => setShowPassword((show) => !show)
+
+  const handleMouseDownPassword = (event) => {
+    event.preventDefault();
+  }
 
   const handleChange = (e) => {
     handler(e.target.value)
@@ -16,11 +25,20 @@ const InputField = ({ placeholder, value, handler, type, required }) => {
         variant="outlined" 
         value={value}
         onChange={handleChange}
-        type={type}
+        type={type === 'password' ? (showPassword ? 'text' : 'password') : 'text'}
         required={required}
         fullWidth={true}
-        InputLabelProps={{
-          className: 'label'
+        InputProps={type === 'password' && {
+          endAdornment: <InputAdornment position="end">
+            <IconButton
+              aria-label="toggle password visibility"
+              onClick={handleClickShowPassword}
+              onMouseDown={handleMouseDownPassword}
+              style={{color: '#8d8d8d'}}
+            >
+              {showPassword ? <VisibilityOff /> : <Visibility />}
+            </IconButton>
+          </InputAdornment>,
         }}
       />
   )
